0e70c6079f14154145b90324e0cf9326aa233b5a
simon-ulrik <simon.ulrik.j@gmail.com>
2021-06-17 00:05:57 +0200

    basic setup

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Simon's Portfolio</title>
+      <h1>Hello!</h1>
+      <h2>My Name Is Simon.</h2>
+      <p>This site is a work in progress right now,</p>
+      <p>check out my insta instead: <a href="https://www.instagram.com/simons.pictures/">@simons.pictures</a></p>
+    </main>
+  )
+}
+
+export default IndexPage
