2dc211f2b80386f78cc0761a5b31d55256445014
simon-ulrik <simon.ulrik.j@gmail.com>
2021-07-16 00:46:56 +0200

    added simple toggle for menu

--- gatsby/src/pages/components/menu.js
+++ gatsby/src/pages/components/menu.js
@@ -1,33 +1,47 @@
-import React from "react"
+import React, { Component } from 'react'
+import "../../styles/menu.css"
 
-export default function Menu(props) {
-  return (
-    <nav>
-        <h2>Menu!!</h2>
-        <div id="nav_btns">
-            <div id="toggle_btn"></div>
-            <h1 id="menu_title">{props.title} då</h1>
-        </div>
-        <ul id="nav_links">
-            <li>Books</li>
-                <ul>
-                    <li>Timon på äventyr</li>
-                    <li>2019 OF SIMON</li>
+export default class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {toggle: "closed"};
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            toggle: (this.state.toggle === "closed") ? "open" : "closed"
+        });
+    }
+
+    render() {
+        return (
+            <nav id="menu">
+                <div id="nav_btns" className={this.state.toggle}>
+                    <div id="toggle_btn" onClick={this.toggle}><h1>Toggle</h1></div>
+                    <h1 id="menu_title"></h1>
+                </div>
+                <ul id="nav_links" className={this.state.toggle}>
+                    <li>Books</li>
+                        <ul>
+                            <li>Timon på äventyr</li>
+                            <li>2019 OF SIMON</li>
+                        </ul>
+                    <li>Imgs slides</li>
+                        <ul>
+                            <li>Simon's Powershot</li>
+                            <li>Groda</li>
+                            <li>STHLM</li>
+                        </ul>
+                    <li>Other imgs</li>
+                        <ul>
+                            <li>Åre/Skiing</li>
+                            <li>Gothenburg</li>
+                            <li>Jönköping</li>
+                        </ul>
+                    <li>About me</li>
                 </ul>
-            <li>Imgs slides</li>
-                <ul>
-                    <li>Simon's Powershot</li>
-                    <li>Groda</li>
-                    <li>STHLM</li>
-                </ul>
-            <li>Other imgs</li>
-                <ul>
-                    <li>Åre/Skiing</li>
-                    <li>Gothenburg</li>
-                    <li>Jönköping</li>
-                </ul>
-            <li>About me</li>
-        </ul>
-    </nav>
-  )
+            </nav>
+        )
+    }
 }
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -9,10 +9,11 @@ const IndexPage = () => {
       <h1>Hello!</h1>
       <h2>My Name Is Simon.</h2>
       <p>This site is a work in progress right now,</p>
-      <p>check out my insta instead: <a href="https://www.instagram.com/simons.pictures/">@simons.pictures</a></p>
+      <p>check out my instagram instead: <a href="https://www.instagram.com/simons.pictures/">@simons.pictures</a></p>
+      <Menu></Menu>
     </main>
 
-    <Menu title="hej"></Menu>
+    
     </>
   )
 }
