8307d5e44ec7e0fa82e11241e4fb2244c587b0c1
simon-ulrik <simon.ulrik.j@gmail.com>
2021-08-17 23:01:58 +0200

    added background image on index page and changed logo and menu to white color

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -28,14 +28,15 @@ export default class IndexPage extends Component {
             <div className="content_middle">
               <div id="logo_container">
                 <StaticImage
-                  src="../images/logo.svg"
+                  src="../images/logo_white.svg"
                   alt="Simon's Pictures"
                   placeholder="none"
                   layout="fullWidth"
+                  id="logo"
                 />
               </div>
             </div>
-            <ul className="nav_links">
+            <ul className="nav_links index">
               {this.navLinks()}
             </ul>
           </div>
