d804ecfd2e3b862b3c99637f46e2fed4bd837479
simon-ulrik <simon.ulrik.j@gmail.com>
2021-09-02 14:12:22 +0200

    added grid for the projects page

--- gatsby/src/pages/projects/index.js
+++ gatsby/src/pages/projects/index.js
@@ -1,15 +1,41 @@
-import * as React from "react"
+import React, { Component } from 'react'
+import { StaticImage } from "gatsby-plugin-image"
+
 import NavBar from "../components/nav_bar.js"
+import PicGrid from "../components/pic_grid"
 
-const Projects = () => {
-  return (
-    <>
-    <NavBar backLink="/"></NavBar>
-    <main className="site_content">
-        <h1>Projects</h1>
-    </main>
-    </>
-  )
+export default class Projects extends Component {
+
+  links() {
+    return ([
+      "/",
+      "/"
+    ])
+  }
+
+  render() {
+    return (
+      <>
+      <NavBar backLink="/"></NavBar>
+      <main className="site_content">
+          <h1 className="main_page_title">Projects</h1>
+          <PicGrid links={this.links()}>
+            <StaticImage
+              src="https://drive.google.com/uc?export=view&id=1pFIi8A1zVnY6x4V3tO-5-U44kHVrF1ed"
+              alt=""
+              placeholder="blurred"
+              className="grid_item"
+            />
+            <StaticImage
+              src="https://images.wagwalkingweb.com/media/articles/care/why-is-my-dog-yawning-and-licking-his-lips/why-is-my-dog-yawning-and-licking-his-lips.jpg"
+              alt=""
+              placeholder="blurred"
+              className="grid_item"
+            />
+          </PicGrid>
+      </main>
+      </>
+    )
+  }
+  
 }
-
-export default Projects
