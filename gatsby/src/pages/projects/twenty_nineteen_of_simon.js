5c5ab394180d2ee57a3afa06c9512814c77ff6f1
simon-ulrik <simon.ulrik.j@gmail.com>
2021-09-04 17:17:08 +0200

    added pictures to twenty_nineteen_of_simon

--- gatsby/src/pages/projects/twenty_nineteen_of_simon.js
+++ gatsby/src/pages/projects/twenty_nineteen_of_simon.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import ProjectPage from '../../templates/project_page.js'
 
 const INFO = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -28,12 +29,98 @@ Ullamcorper eget nulla facilisi etiam dignissim.
 Commodo ullamcorper a lacus vestibulum sed.
 Volutpat ac tincidunt vitae semper quis.`
 
+const SPREADS = [
+  <StaticImage
+    src="https://drive.google.com/uc?export=view&id=1xE3f-qpix27tDPfC7MmBtdL3MDQVTcem"
+    alt=""
+    placeholder="blurred"
+    key={1}
+  />,
+  [
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=18W4nGu-ueZB5uYdsZKlfjarPwVsWG7__"
+      alt=""
+      placeholder="blurred"
+      key={2}
+    />,
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1kyO6gQV9MwxNUWhvyfKRSu2FilKMNQ3k"
+      alt=""
+      placeholder="blurred"
+      key={3}
+    />
+  ],
+  [
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1YO8XZEREauVejuNGMFpJovPbGLCHFwIf"
+      alt=""
+      placeholder="blurred"
+      key={4}
+    />,
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1mRnbPH9FE7_OeH9jM9CONdLiRNEmu2Pl"
+      alt=""
+      placeholder="blurred"
+      key={5}
+    />
+  ],
+  [
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1mvaXKaq8Ma-yGr8NFNujWl2mYEIVtIUl"
+      alt=""
+      placeholder="blurred"
+      key={6}
+    />,
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1fzIElxaqS-Obkl0SXUqGIb_zkI__mgvO"
+      alt=""
+      placeholder="blurred"
+      key={7}
+    />
+  ],
+  [
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1Vpz5VzsLRWFSrSQp9OEmB2UyfSf2vtDu"
+      alt=""
+      placeholder="blurred"
+      key={8}
+    />,
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1zUto3tGXwqxTNVRewQxeeUT4QmL0jfvK"
+      alt=""
+      placeholder="blurred"
+      key={9}
+    />
+  ],
+  [
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1l1gZLY1Jh2fXc7wqZ6msoEpij9YcN0k0"
+      alt=""
+      placeholder="blurred"
+      key={10}
+    />,
+    <StaticImage
+      src="https://drive.google.com/uc?export=view&id=1a-lydvM6nqf1IJwLOCOhkGLhdaDMdzar"
+      alt=""
+      placeholder="blurred"
+      key={11}
+    />
+  ],
+  <StaticImage
+    src="https://drive.google.com/uc?export=view&id=1Akng-qWLhTjjDgeOk2p6g8MbeumDvu9w"
+    alt=""
+    placeholder="blurred"
+    key={12}
+  />
+]
+
 const twenty_nineteen_of_simon = () => {
   return (
     <ProjectPage
       info={{title: "2019 OF SIMON", content: INFO}}
+      type="book"
     >
-      
+      {SPREADS}
     </ProjectPage>
   )
 }
