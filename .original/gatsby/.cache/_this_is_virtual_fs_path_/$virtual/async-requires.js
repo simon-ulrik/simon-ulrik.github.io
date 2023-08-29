d7cd83019ccccde65f236e3817f0128dc5a4cd92
Simon Johannesson <simon.ulrik.j@gmail.com>
2023-08-29 10:31:48 +0200

    test new site

---
+++ .original/gatsby/.cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,17 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js?export=default" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-projects-index-js": () => import("./../../../src/pages/projects/index.js?export=default" /* webpackChunkName: "component---src-pages-projects-index-js" */),
+  "component---src-pages-projects-timon-pa-aventyr-js": () => import("./../../../src/pages/projects/timon_pa_aventyr.js?export=default" /* webpackChunkName: "component---src-pages-projects-timon-pa-aventyr-js" */),
+  "component---src-pages-projects-twenty-nineteen-of-simon-js": () => import("./../../../src/pages/projects/twenty_nineteen_of_simon.js?export=default" /* webpackChunkName: "component---src-pages-projects-twenty-nineteen-of-simon-js" */)
+}
+
+
+
+exports.head = {
+
+}
+
---
+++ .original/gatsby/.cache/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/about.js")),
+  "component---src-pages-contact-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/contact.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/index.js")),
+  "component---src-pages-projects-index-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/projects/index.js")),
+  "component---src-pages-projects-timon-pa-aventyr-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/projects/timon_pa_aventyr.js")),
+  "component---src-pages-projects-twenty-nineteen-of-simon-js": preferDefault(require("/home/simon/github/simon-ulrik.github.io/gatsby/src/pages/projects/twenty_nineteen_of_simon.js"))
+}
+
