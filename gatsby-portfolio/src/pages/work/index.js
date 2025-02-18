cb1cbaf19b0009ac86277541e9b872ed46426420
Simon Johannesson <simon.ulrik.j@gmail.com>
2025-02-19 00:49:16 +0100

    idn

--- gatsby-portfolio/src/pages/work/index.js
+++ gatsby-portfolio/src/pages/work/index.js
@@ -2,6 +2,14 @@ import * as React from "react"
 import Layout from "../../components/Layout"
 
 export default function Work(props) {
+
+  const pages = [
+    ["Enclosed Space", "enclosed-space"],
+    ["Alla pizzerior i Göteborg (exhibition)", "alla-pizzerior-i-goteborg-exhibition"],
+    ["Närmre dig", "narmre-dig"]
+    ["A year in 27.7 seconds", "a-year-in-27_7-seconds"]
+  ];
+
   return (
     <Layout location={props.location}>
       <section>
