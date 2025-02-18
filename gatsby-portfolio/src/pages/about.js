93ff5f3fc4b324cb5936399ec15e793a86a75417
Simon Johannesson <simon.ulrik.j@gmail.com>
2025-02-19 00:26:24 +0100

    added info on about page

--- gatsby-portfolio/src/pages/about.js
+++ gatsby-portfolio/src/pages/about.js
@@ -4,7 +4,50 @@ import Layout from "../components/Layout"
 export default function about(props) {
   return (
     <Layout location={props.location}>
-      <div>about</div>
+      <div>
+        <h1>Simon Johannesson</h1>
+        <h2>About</h2>
+        <p>
+        Simon Johannessons fotografier har ett dokumentärt perspektiv och präglas av intresse för teknik, material och vardagsliv. Hans fotoboksprojekt "Enclosed space" utforskar samspelet mellan människan, samhället och industri genom svartvita fotografier från industriella miljöer. Han dokumenterar även vardagsestetik i gatufotografi och offentliga miljöer, som i det pågående projektet "Alla pizzerior i Göteborg", där han systematiskt arkiverar stadens fristående pizzerior. Simon Johannesson, född 1999, är en svensk fotograf baserad i Göteborg och studerar fotografi vid HDK-Valand.
+        </p>
+        <h2>CV</h2>
+        <h3>Soloutställningar</h3>
+        <p>
+        2024: "Alla pizzerior i Göteborg: WORK IN PROGRESS" | Frilagret, Göteborg | 1 — 19 okt.<br/>
+        2024: "Närmre dig" | Frilagret, Göteborg | 30 jan. — 17 feb.
+        </p>
+        <h3>Grupputställningar</h3>
+        <p>
+        2025: "Bibliotekssmälta: Zines & Tomes" | Konstbiblioteket, Göteborg | 10 — 14 feb.<br/>
+        2025: "This must be the place" | Litteraturhuset, Göteborg | 31 jan. — 2 feb.<br/>
+        2025: "Sticky Floor, Blinding Lights" | Yaki Da, Göteborg | jan.<br/>
+        2024: "Take 10 Press Liberary showcase" | Konstkollektivet Mölndal | nov.<br/>
+        2024: "Gothenburg street photo festival" |  Aftonstjärnan, Göteborg | sep.<br/>
+        2024: "BA1 Photography" | Street Gallery, Göteborg | 29 maj — 16 sep.<br/>
+        2024: "Arkiv" | Tripp & Trapp, Göteborg | maj<br/>
+        2024: "Take 10 Press Liberary showcase" | Skogen, Göteborg | mars<br/>
+        2023: "Gothenburg street photo festival" | Vega Bryggeri, Göteborg | sep.<br/>
+        2023: "Snickeriet streets" | Stockholm | aug.<br/>
+        2022: "Gothenburg street photo festival" | Vega Bryggeri, Göteborg | sep.<br/>
+        2022: "Punktum" | The Secret Door, Göteborg | 20 — 22 maj<br/>
+        2022: "Härma" | Mimers kulturhus, Kungälv | mars<br/>
+        2022: "Dystopi" | Nordiska folkhögskolan, Kungälv | feb.<br/>
+        2021: "Självporträtt" | Nordiska folkhögskolan, Kungälv | 28 okt. — 10 nov.<br/>
+        2021: "Gothenburg street photo festival" | Vega Bryggeri, Göteborg | sep.
+        </p>
+        <h3>Tävlingar</h3>
+        <p>
+        2022: "Gothenburg street photo festival" | Första plats | Kategori: “Under 30”<br/>
+        2023: "Gothenburg street photo festival" | Andra plats | Kategori: “Under 30”<br/>
+        2024: "Gothenburg street photo festival" | Tredje plats | Kategori: “Under 30”
+        </p>
+        <h3>Utbildning</h3>
+        <p>
+        2023 — 2026: Konstnärlig kandidat i fotografi | HDK-Valand, Göteborgs Universitet<br/>
+        2019 — 2021, 2022 — 2023: Kandidatexamen i Maskinteknik | Jönköpings tekniska högskola (Jönköping University)<br/>
+        2021 — 2022 Folkhögskoleutbildning i Fotografi | Nordiska fotoskolan
+        </p>
+      </div>
     </Layout>
   )
 }
